32ea44690f05ec9df24ab8d01d736527726ed70e
mbuffetti <mattia.buffetti@gmail.com>
2026-05-07 16:43:11 +0200

    Implement authentication with NextAuth and user role management

--- app/api/tales/create/route.ts
+++ app/api/tales/create/route.ts
@@ -1,25 +1,25 @@
 import { NextRequest, NextResponse } from "next/server";
 
-import { createUser } from "@/api/controllers/user-db";
-import { CreateUserSchema } from "@/schemas/user";
+import { createTale } from "@/api/controllers/tales-db";
+import { CreateTaleSchema } from "@/schemas/tales";
 
 export async function POST(req: NextRequest): Promise<NextResponse> {
   try {
     const body = (await req.json()) as Record<string, unknown>;
-    body.role = "6551f7cc8f006751fc52a52b";
-    const parsed = CreateUserSchema.safeParse(body);
+    console.log(body)
+    const parsed = CreateTaleSchema.safeParse(body);
     if (!parsed.success) {
       return NextResponse.json(
         { error: parsed.error.issues, success: false },
         { status: 400 }
       );
     }
-    const user = await createUser(parsed.data);
+    const tale = await createTale(parsed.data);
 
-    if ("error" in user) {
-      return NextResponse.json({ ...user, success: false }, { status: 500 });
+    if ("error" in tale) {
+      return NextResponse.json({ ...tale, success: false }, { status: 500 });
     }
-    return NextResponse.json(user);
+    return NextResponse.json(tale);
   } catch (err) {
     return NextResponse.json({ error: String(err) }, { status: 500 });
   }
