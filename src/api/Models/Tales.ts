c80e386dc1b9820bea40ce48b5e1e7fcd2e12076
mbuffetti <mattia.buffetti@gmail.com>
2026-05-19 15:21:31 +0100

    feat: add type field to Tales schema for improved categorization

--- src/api/Models/Tales.ts
+++ src/api/Models/Tales.ts
@@ -22,6 +22,7 @@ const TaleMongooseSchema = new Schema<Itale>(
         title: { type: String, required: true },
         description: { type: String, required: true },
         order: { type: Number, default: 0 },
+        type: { type: String, required: true },
         CoverImage: {
             url: String,
             path: String,
